eea0ad353ac79f310b3144b252d28efda3d499e5
sameer <sameer.easyr@gmail.com>
2022-11-24 10:51:42 +0530

    updated new files

--- WaferFaultDetection/code/WaferFaultDetection_new/trainingModel.py
+++ WaferFaultDetection/code/WaferFaultDetection_new/trainingModel.py
@@ -3,7 +3,7 @@ This is the Entry point for Training the Machine Learning Model.
 
 Written By: Samir Shaikh
 Version: 1.0
-Revisions: None
+Revisions: First
 
 """
 
@@ -11,7 +11,7 @@ Revisions: None
 # Doing the necessary imports
 from sklearn.model_selection import train_test_split
 from data_ingestion import data_loader
-from data_preprocessing import preprocessing
+# from data_preprocessing import preprocessing
 from data_preprocessing import clustering
 from best_model_finder import tuner
 from file_operations import file_methods
